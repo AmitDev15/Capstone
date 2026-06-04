385c0ea6faea3e57dcfde92f3051e1ee867d8f49
Amit Kumar <amitk.singh1506@gmail.com>
2026-06-04 21:51:19 +0530

    - Added sandbox-service. -Implemented templates for frontend -Added functionality that sandbox-service can create separate pods and service for a each user.

---
+++ sandbox/server/src/app.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import morgan from 'morgan'
+import { createPod } from './kubernetes/pod.js'
+import { createService } from './kubernetes/service.js'
+import { v7 as uuid } from 'uuid'
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/api/sandbox/health', (req, res) => {
+  res.status(200).json({
+    status: 'ok',
+    message: 'Sandbox API is healthy',
+  })
+})
+
+app.post('/api/sandbox/start', async (req, res) => {
+  try {
+    const sandboxId = uuid()
+
+    await Promise.all([createPod(sandboxId), createService(sandboxId)])
+
+    return res.status(201).json({
+      status: 'success',
+      message: 'Sandbox environment created successfully',
+      sandboxId,
+      previewUrl: `http://${sandboxId}.preview.localhost`,
+    })
+  } catch (error) {
+    console.error(error)
+
+    return res.status(500).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+})
+
+export default app
